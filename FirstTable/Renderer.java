4c1f27df7de460188a3f2ba38747f99d6a04807e
Dvimin <dmitriev.michail@mail.ru>
2024-06-17 19:07:35 +0300

    add render.java

---
+++ FirstTable/Renderer.java
@@ -0,0 +1,15 @@
+package FirstTable;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+
+public class Renderer extends DefaultTableCellRenderer {
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+        cell.setBackground(Color.LIGHT_GRAY);
+        return cell;
+    }
+}
