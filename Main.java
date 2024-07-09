88b9d303e45ea82325ba612c10bb52f4d1329cf7
Dvimin <115354604+Dvimin@users.noreply.github.com>
2024-07-09 10:34:34 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    
-    public static void main(String[] args) {
-        SimpleGUI2 app = new SimpleGUI2();
-        app.setVisible(true);
-        System.out.println("Hello, World!");
-
-    }
-}
