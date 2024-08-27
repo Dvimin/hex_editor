911aa3beebe3a8149824923a7af06eafcbde7fe2
dmitriev.michail@mail.ru <dmitriev.michail@mail.ru>
2024-08-27 14:27:34 +0300

    reset setupTestFile in FrameSetup

--- src/main/java/ui/FrameSetup.java
+++ src/main/java/ui/FrameSetup.java
@@ -1,19 +1,10 @@
 package ui;
-
-import actions.FileActions;
-import ui.BinTableModel;
-import ui.MenuSetup;
-import ui.TableSetup;
-
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-
 public class FrameSetup {
-
-
     // Настройка основного фрейма приложения.
     public void setupFrame() {
         JFrame frame = new JFrame("HEX-editor");
@@ -21,20 +12,29 @@ public class FrameSetup {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new GridBagLayout());
-
         BinTableModel btm = new BinTableModel();
-        FileActions fileActions = new FileActions();
-        fileActions.setupTestFile(btm); // Вызов метода для загрузки тестового файла
-
+        setupTestFile(btm); // Вызов метода для загрузки тестового файла
         TableSetup tableSetup = new TableSetup();
         tableSetup.setupTable(frame, btm);
-
         MenuSetup menuSetup = new MenuSetup();
         menuSetup.setupMenu(frame, btm, tableSetup);
-
         frame.setVisible(true);
         frame.pack();
     }
 
+    // Метод для загрузки тестового файла в модель таблицы. Может быть удален при необходимости.
+    private void setupTestFile(BinTableModel btm) {
+        File file = new File("src/main/resources/test.txt");
+        byte[] hex = new byte[16];
 
+        try {
+            byte[] data = Files.readAllBytes(file.toPath());
+            for (int i = 0; i < data.length; i += 16) {
+                System.arraycopy(data, i, hex, 0, Math.min(16, data.length - i));
+                btm.addData(hex);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
