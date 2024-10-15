8d6dec2bf83a256bd4bdbb991f97349cb43f38a3
Dvimin <dmitriev.michail@mail.ru>
2024-10-15 17:14:36 +0300

    Edited auto-save and auto-opening - now they are working correctly - the second copy of the backup file is not deleted

--- src/main/java/actions/FileActions.java
+++ src/main/java/actions/FileActions.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 import java.nio.file.*;
 
 public class FileActions {
@@ -70,10 +72,16 @@ public class FileActions {
         System.out.println("Бэкап-файл создан.");
     }
 
-    // Открытие файла с определённой страницы
-    public void openFileAtPage(BinTableModel btm) {
-        if (currentFile != null) {
-            try {
+    // Автоматическое открытие файла с определённой страницы
+    public void autoOpenFileAtPage(BinTableModel btm) {
+        Path backupDirectoryPath = Paths.get("backup");
+        Path backupFilePath = backupDirectoryPath.resolve("backup.txt");
+        File backupFile = backupFilePath.toFile();
+
+        if (backupFile.exists()) {
+            try (RandomAccessFile raf = new RandomAccessFile(backupFile, "r");
+                 FileChannel fileChannel = raf.getChannel();
+                 FileLock lock = fileChannel.lock(0, Long.MAX_VALUE, true)) {
                 openAndLoadFileAtPage(btm, currentPage);
             } catch (IOException e) {
                 showErrorDialog(null, "Ошибка чтения файла: ", e);
@@ -86,7 +94,10 @@ public class FileActions {
 
     // Открытие файла и загрузка указанной страницы
     private void openAndLoadFileAtPage(BinTableModel btm, int pageNumber) throws IOException {
-        raf = new RandomAccessFile(currentFile, "r");
+        if (raf != null) {
+            raf.close();
+        }
+        raf = new RandomAccessFile(backupFile, "r");
         totalPages = calculateTotalPages();
         if (pageNumber >= 0 && pageNumber < totalPages) {
             loadPage(btm, pageNumber);
@@ -96,35 +107,79 @@ public class FileActions {
         }
     }
 
-
-    // Автоматическое сохранение текущей страницы, включая контекст, без создания нового файла
-    public void autoSaveCurrentPageWithContext(BinTableModel btm) {
+    // Автоматическое сохранение всех стариц
+    public void autoSaveFileAtPage(BinTableModel btm) {
         Path backupDirectoryPath = Paths.get("backup");
         Path backupFilePath = backupDirectoryPath.resolve("backup.txt");
+        Path tempBackupFilePath = backupDirectoryPath.resolve("backup2.txt");
         File backupFile = backupFilePath.toFile();
-        if (!backupFile.exists()) {
-            System.out.println("Файл бэкапа по пути не найден: " + backupFile.getAbsolutePath());
-            return;
-        }
-        try (FileOutputStream fos = new FileOutputStream(backupFile);
+        File tempBackupFile = tempBackupFilePath.toFile();
+
+        try (FileOutputStream fos = new FileOutputStream(tempBackupFile);
              BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
             for (int pageNumber = 0; pageNumber < currentPage; pageNumber++) {
                 byte[] pageData = loadPageData(pageNumber);
-                bos.write(pageData);
+                buffer.write(pageData);
             }
+
             byte[] currentPageData = btm.getAllData();
-            bos.write(currentPageData);
+            buffer.write(currentPageData);
+
             for (int pageNumber = currentPage + 1; pageNumber < totalPages; pageNumber++) {
                 byte[] pageData = loadPageData(pageNumber);
-                bos.write(pageData);
+                buffer.write(pageData);
             }
+
+            bos.write(buffer.toByteArray());
             bos.flush();
-            System.out.println("Файл бэкапа успешно перезаписан: " + backupFile.getAbsolutePath());
+            System.out.println("Временный файл бэкапа успешно записан: " + tempBackupFile.getAbsolutePath());
+
+            try (FileInputStream fis = new FileInputStream(tempBackupFile);
+                 FileOutputStream backupFos = new FileOutputStream(backupFile);
+                 BufferedOutputStream backupBos = new BufferedOutputStream(backupFos)) {
+
+                byte[] bufferCopy = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = fis.read(bufferCopy)) != -1) {
+                    backupBos.write(bufferCopy, 0, bytesRead);
+                }
+
+                backupBos.flush();
+                System.out.println("Файл бэкапа успешно обновлён: " + backupFile.getAbsolutePath());
+
+            } catch (IOException e) {
+                showErrorDialog(null, "Ошибка при копировании данных во временный файл бэкапа: ", e);
+            }
+
+            System.out.println("Проверяем возможность удаления временного файла: " + tempBackupFile.getAbsolutePath());
+            if (tempBackupFile.exists()) {
+                System.out.println("Временный файл существует и его размер: " + tempBackupFile.length() + " байт");
+
+                if (tempBackupFile.delete()) {
+                    System.out.println("Временный файл успешно удалён.");
+                } else {
+                    System.out.println("Не удалось удалить временный файл. Причина: файл может быть занят другим процессом или у приложения нет прав.");
+                }
+            } else {
+                System.out.println("Временный файл не существует перед попыткой удаления.");
+            }
+            try {
+                Files.delete(tempBackupFilePath);
+                System.out.println("Временный файл успешно удалён.");
+            } catch (NoSuchFileException e) {
+                System.out.println("Временный файл не существует.");
+            } catch (DirectoryNotEmptyException e) {
+                System.out.println("Директория не пуста: " + e.getFile());
+            } catch (IOException e) {
+                System.out.println("Не удалось удалить временный файл. Причина: " + e.getMessage());
+            }
+
         } catch (IOException e) {
-            showErrorDialog(null, "Ошибка при перезаписи файла бэкапа: ", e);
+            showErrorDialog(null, "Ошибка при записи временного файла: ", e);
         }
     }
-
 
     // Метод для очистки папки backup
     private void clearBackupDirectory(Path backupDir) throws IOException {
@@ -143,6 +198,9 @@ public class FileActions {
 
     // Открытие файла и расчет количества страниц
     private void openAndLoadFile(BinTableModel btm) throws IOException {
+        if (raf != null) {
+            raf.close();
+        }
         raf = new RandomAccessFile(currentFile, "r");
         totalPages = calculateTotalPages();
         loadPage(btm, 0);
@@ -152,9 +210,11 @@ public class FileActions {
     private void loadPage(BinTableModel btm, int pageNumber) throws IOException {
         btm.clearData();
         long offset = pageNumber * PAGE_SIZE;
+        long currentPosition = raf.getFilePointer();
         raf.seek(offset);
         byte[] pageData = new byte[PAGE_SIZE];
         int bytesRead = raf.read(pageData);
+        raf.seek(currentPosition);
         if (bytesRead > 0) {
             btm.addData(trimData(pageData, bytesRead));
         }
@@ -243,10 +303,25 @@ public class FileActions {
 
     // Метод для загрузки данных страницы без обновления UI
     private byte[] loadPageData(int pageNumber) throws IOException {
+        if (raf == null) {
+            throw new IllegalStateException("RandomAccessFile не был инициализирован.");
+        }
+        if (pageNumber < 0 || pageNumber >= totalPages) {
+            throw new IllegalArgumentException("Неправильный номер страницы: " + pageNumber);
+        }
         long offset = pageNumber * PAGE_SIZE;
-        raf.seek(offset);
         byte[] pageData = new byte[PAGE_SIZE];
-        int bytesRead = raf.read(pageData);
+        int bytesRead;
+        try {
+            raf.seek(offset);
+            bytesRead = raf.read(pageData);
+        } catch (IOException e) {
+            throw new IOException("Ошибка чтения файла на странице: " + pageNumber, e);
+        }
+
+        if (bytesRead == -1) {
+            throw new IOException("Достигнут конец файла!");
+        }
         return trimData(pageData, bytesRead);
     }
 
