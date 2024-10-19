04033e4f39c1f64c21e923a3732e4bb2429a59e4
Dvimin <dmitriev.michail@mail.ru>
2024-10-19 21:48:47 +0300

    Resized the application window

--- src/main/java/ui/FrameSetup.java
+++ src/main/java/ui/FrameSetup.java
@@ -4,28 +4,25 @@ import actions.FileActions;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 
 public class FrameSetup {
     // Настройка основного фрейма приложения.
     public void setupFrame() {
         JFrame frame = new JFrame("HEX-editor");
-        frame.setSize(new Dimension(1000, 400));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
         frame.setLayout(new GridBagLayout());
 
         BinTableModel btm = new BinTableModel();
         FileActions fileActions = new FileActions();
-        fileActions.createAndOpenInitialFile(btm); // Вызов метода для загрузки тестового файла
+        fileActions.createAndOpenInitialFile(btm);
 
         TableSetup tableSetup = new TableSetup();
         tableSetup.setupTable(frame, btm);
         MenuSetup menuSetup = new MenuSetup();
         menuSetup.setupMenu(frame, btm, tableSetup);
+
+        frame.setSize(new Dimension(1100, 400));
         frame.setVisible(true);
-        frame.pack();
+        frame.setLocationRelativeTo(null);
     }
 }
